91b7404fcdb2bc1bbb164230149233453cd0640c
akhanche <79458156+akhanche@users.noreply.github.com>
2021-03-09 22:33:00 +0000

    en

--- stage0.py
+++ stage0.py
@@ -1,9 +1,36 @@
-from env import config
 import requests
-url = "https://webexapis.com/v1/rooms"
-headers = {'Authorization': f"Bearer {config['WEBEX_ACCESS_TOKEN']}"}
-response = requests.get(url, headers=headers).json()
+from requests.auth import HTTPBasicAuth
 
-for i in response['items']:
-    if i['title'] == 'CSAP Programmability CTF - Team 2':
-        print("Room ID is :" ,i['id'])
+from env import config
+
+headers = {
+    "X-Cisco-Meraki-API-Key": config['MERAKI_KEY']
+}
+
+orgs_url = f"{config['MERAKI_BASE_URL']}/organizations"
+resp = requests.get(orgs_url, headers=headers)
+
+
+if resp.status_code == 200:
+    print("Meraki Access verified")
+else:
+    print(f"Meraki status code: {resp.status_code}")
+
+
+
+#url = "https://api.meraki.com/api/v0/organizations"
+
+payload = None
+
+headers = {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+    "X-Cisco-Meraki-API-Key": config['MERAKI_KEY']
+}
+
+response = requests.request('GET', orgs_url, headers=headers, data = payload).json()
+
+#print(response.text.encode('utf8'))
+
+for org in response:
+    print("orgId:" + org["id"], "name:" + org["name"])    
